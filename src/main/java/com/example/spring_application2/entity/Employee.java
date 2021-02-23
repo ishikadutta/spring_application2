61445e90722730dc2348bdd12c9a37a8a289ea41
Ishika Dutta <ishikadutta@Ishikas-MacBook-Pro.local>
2021-02-23 17:50:42 +0530

    mongo

--- src/main/java/com/example/spring_application2/entity/Employee.java
+++ src/main/java/com/example/spring_application2/entity/Employee.java
@@ -2,20 +2,24 @@ package com.example.spring_application2.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
+//import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
 
-@Entity
+//@Entity
 @Getter
 @Setter
+@Document
 public class Employee {
-    @Id
-    @GenericGenerator(name="employee_id_seq", strategy="increment")
-    @GeneratedValue(generator = "employee_id_seq",strategy = GenerationType.AUTO)
+//    @Id
+//    @GenericGenerator(name="employee_id_seq", strategy="increment")
+//    @GeneratedValue(generator = "employee_id_seq",strategy = GenerationType.AUTO)
+    @MongoId
     private Long id;
     private String name;
     private String departmentName;
